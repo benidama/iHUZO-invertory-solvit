fa75ddfd762e7fa5ebedc210e0fa517eb896556e
Benimana Jean Damascene <benidama02@gmail.com>
2025-07-25 13:33:06 -0700

    setup of basic packages of my project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <>
+      <h1 className="text-6xl">React vite</h1>
+      <p className="text-amber-100">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+};
+
+export default App;
